359144319f177dcf244b79c088079ec04a069af9
thiagodesa-cell <thiago.desa@yahoo.com.br>
2026-08-11 20:25:29 -0300

    Change API_KEY_FIXA to new key
    Updated API key for compatibility with new standards.

--- painel.py
+++ painel.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title="Painel Pro - Global Trading & IA Preditiva v22.2"
 
 # --- CONFIGURAÇÃO DA API E TELEGRAM ---
 # Compatível com chaves antigas (AIza) ou novas (HAQ)
-API_KEY_FIXA = "AQ.Ab8RN6L-h6_cjeQe4v9pSwQq8tzG-N407YZY4ixRGurNuX6yJA" 
+API_KEY_FIXA = "AQ.Ab8RN6KbxK2aY4CM4pUSboCLjrL0xCSks0dxTGn1v7KWZLKM6A" 
 SEASON = datetime.now().year 
 
 TELEGRAM_TOKEN = "8281259090:AAEggXJKpCMxRbhhrcCZymcmNUKWNoOPFfY"
